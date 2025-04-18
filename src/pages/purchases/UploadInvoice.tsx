1df80d18735fc7d5495ce7979944d804eaeb8d60
fxrdhan <firdausarief65@gmail.com>
2025-04-18 14:45:57 +0700

    fix(UploadInvoice): change button text to 'Ekspor Data'

--- src/pages/purchases/UploadInvoice.tsx
+++ src/pages/purchases/UploadInvoice.tsx
@@ -156,7 +156,7 @@ const UploadInvoice = () => {
                     {error && (
                         <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4 flex items-center">
                             <svg className="h-5 w-5 mr-2 text-red-500" fill="currentColor" viewBox="0 0 20 20">
-                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 001.414-1.414L11.414 10l1.293-1.293a1 1 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 101.414 1.414L10 11.414l1.293 1.293a1 1 001.414-1.414L11.414 10l1.293-1.293a1 1 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
                             </svg>
                             {error}
                         </div>
@@ -258,7 +258,7 @@ const UploadInvoice = () => {
                                 className="bg-blue-600 hover:bg-blue-700 text-white"
                                 aria-live="polite"
                             >
-                                <FaUpload className="mr-2" /> Unggah & Lanjutkan
+                                <FaUpload className="mr-2" /> Ekspor Data 
                             </Button>
                         </div>
                     </div>
