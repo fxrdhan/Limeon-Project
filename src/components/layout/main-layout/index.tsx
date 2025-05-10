37ce647a8cea3da32401218bb7f78f9c5c907b8c
fxrdhan <firdausarief65@gmail.com>
2025-05-10 22:32:11 +0700

    refactor: move Navbar component to a new file and update import path

--- src/components/layout/main-layout/index.tsx
+++ src/components/layout/main-layout/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "../Navbar";
+import Navbar from "../navbar";
 import Sidebar from "../Sidebar";
 
 const MainLayout = () => {
