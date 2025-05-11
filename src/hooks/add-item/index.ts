427d18d825234eb00d3c93d027e9ecf6ba1c848a
fxrdhan <firdausarief65@gmail.com>
2025-05-11 23:23:44 +0700

    refactor(structure): reorganize add-item hooks and helper functions

--- src/hooks/add-item/index.ts
+++ src/hooks/add-item/index.ts
@@ -6,7 +6,7 @@ import { useUnitConversion } from "@/hooks/unit-conversion";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { formatRupiah, extractNumericValue } from "@/lib/formatters";
 import type { Category, MedicineType, Unit, FormData } from '@/types';
-import { generateTypeCode, generateUnitCode, generateCategoryCode, getUnitById } from "@/hooks/add-item-form/helper";
+import { generateTypeCode, generateUnitCode, generateCategoryCode, getUnitById } from "@/hooks/add-item/helper";
 
 export const useAddItemForm = (itemId?: string) => {
     const navigate = useNavigate();
