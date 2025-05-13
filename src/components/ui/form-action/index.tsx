507af0b70fd22223cdc775999a5317c52eb07a06
fxrdhan <firdausarief65@gmail.com>
2025-05-13 15:29:24 +0700

    refactor(imports): update imports from `@/components/ui/...` to use the barrel file `@/components/ui`.

--- src/components/ui/form-action/index.tsx
+++ src/components/ui/form-action/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui';
 import { FaSave, FaTimes } from 'react-icons/fa';
 import type { FormActionProps } from '@/types';
 
