4d90cb23cb88d7823852e6b5d1d90b5e9b3ea324
fxrdhan <firdausarief65@gmail.com>
2025-05-13 15:32:19 +0700

    refactor(imports): update imports from `@/components/ui/...` to use the barrel file `@/components/ui`.

--- src/components/ui/pagination/index.tsx
+++ src/components/ui/pagination/index.tsx
@@ -1,6 +1,6 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui";
 import { classNames } from "@/lib/classNames";
-import type { PaginationProps } from '@/types';
+import type { PaginationProps } from "@/types";
 
 export const Pagination = ({
     currentPage,
@@ -12,36 +12,47 @@ export const Pagination = ({
 }: PaginationProps) => {
     const handleItemsPerPageClick = (value: number) => {
         const event = {
-            target: { value: value.toString() }
+            target: { value: value.toString() },
         } as React.ChangeEvent<HTMLSelectElement>;
         onItemsPerPageChange(event);
     };
 
     return (
-        <div className={classNames("flex justify-between items-center mt-4 gap-4", className)}>
+        <div
+            className={classNames(
+                "flex justify-between items-center mt-4 gap-4",
+                className
+            )}
+        >
             <div className="flex items-center rounded-full bg-zinc-100 p-1 shadow-md text-gray-700 overflow-hidden">
-                <button 
+                <button
                     className={classNames(
-                        "px-3 py-1.5 rounded-full focus:outline-none transition-all duration-300 ease-in-out", 
-                        itemsPerPage === 10 ? "bg-blue-500 text-white font-medium shadow-sm transform scale-105" : "hover:bg-blue-100 hover:text-blue-600"
+                        "px-3 py-1.5 rounded-full focus:outline-none transition-all duration-300 ease-in-out",
+                        itemsPerPage === 10
+                            ? "bg-blue-500 text-white font-medium shadow-sm transform scale-105"
+                            : "hover:bg-blue-100 hover:text-blue-600"
                     )}
                     onClick={() => handleItemsPerPageClick(10)}
                 >
                     {itemsPerPage === 10 ? "10 items" : "10"}
                 </button>
-                <button 
+                <button
                     className={classNames(
-                        "px-3 py-1.5 rounded-full focus:outline-none transition-all duration-300 ease-in-out", 
-                        itemsPerPage === 20 ? "bg-blue-500 text-white font-medium shadow-sm transform scale-105" : "hover:bg-blue-100 hover:text-blue-600"
+                        "px-3 py-1.5 rounded-full focus:outline-none transition-all duration-300 ease-in-out",
+                        itemsPerPage === 20
+                            ? "bg-blue-500 text-white font-medium shadow-sm transform scale-105"
+                            : "hover:bg-blue-100 hover:text-blue-600"
                     )}
                     onClick={() => handleItemsPerPageClick(20)}
                 >
                     {itemsPerPage === 20 ? "20 items" : "20"}
                 </button>
-                <button 
+                <button
                     className={classNames(
-                        "px-3 py-1.5 rounded-full focus:outline-none transition-all duration-300 ease-in-out", 
-                        itemsPerPage === 40 ? "bg-blue-500 text-white font-medium shadow-sm transform scale-105" : "hover:bg-blue-100 hover:text-blue-600"
+                        "px-3 py-1.5 rounded-full focus:outline-none transition-all duration-300 ease-in-out",
+                        itemsPerPage === 40
+                            ? "bg-blue-500 text-white font-medium shadow-sm transform scale-105"
+                            : "hover:bg-blue-100 hover:text-blue-600"
                     )}
                     onClick={() => handleItemsPerPageClick(40)}
                 >
@@ -55,7 +66,12 @@ export const Pagination = ({
                     size="sm"
                     onClick={() => onPageChange(currentPage - 1)}
                     disabled={currentPage === 1}
-                    className={classNames("px-3 py-1.5 rounded-full text-white focus:ring-0 focus:outline-none transition-all duration-300", currentPage === 1 ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-600")}
+                    className={classNames(
+                        "px-3 py-1.5 rounded-full text-white focus:ring-0 focus:outline-none transition-all duration-300",
+                        currentPage === 1
+                            ? "opacity-50 cursor-not-allowed"
+                            : "hover:bg-blue-600"
+                    )}
                 >
                     &lt;
                 </Button>
@@ -69,7 +85,12 @@ export const Pagination = ({
                     size="sm"
                     onClick={() => onPageChange(currentPage + 1)}
                     disabled={currentPage === totalPages || totalPages === 0}
-                    className={classNames("px-3 py-1.5 rounded-full text-white focus:ring-0 focus:outline-none transition-all duration-300", (currentPage === totalPages || totalPages === 0) ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-600")}
+                    className={classNames(
+                        "px-3 py-1.5 rounded-full text-white focus:ring-0 focus:outline-none transition-all duration-300",
+                        currentPage === totalPages || totalPages === 0
+                            ? "opacity-50 cursor-not-allowed"
+                            : "hover:bg-blue-600"
+                    )}
                 >
                     &gt;
                 </Button>
