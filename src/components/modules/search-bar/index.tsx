0a34aaf11b097828072fda1ef2e93e0bf6d61d86
fxrdhan <firdausarief65@gmail.com>
2025-05-19 04:13:10 +0700

    style(search-bar): change input border radius from rounded-md to rounded-lg

--- src/components/modules/search-bar/index.tsx
+++ src/components/modules/search-bar/index.tsx
@@ -13,7 +13,7 @@ export const SearchBar: React.FC<TableSearchProps> = ({
             <input
                 type="text"
                 placeholder={placeholder}
-                className="w-full p-2 border rounded-md pl-10 focus:outline-none focus:border-primary focus:ring-2 focus:ring-blue-300"
+                className="w-full p-2 border rounded-lg pl-10 focus:outline-none focus:border-primary focus:ring-2 focus:ring-blue-300"
                 value={value}
                 onChange={onChange} />
             <FaSearch className="absolute left-3 top-3.5 text-gray-400" />
