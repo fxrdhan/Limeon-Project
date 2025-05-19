af252a96ac9cf938a29e8a7435297907f3d72607
fxrdhan <firdausarief65@gmail.com>
2025-05-19 21:00:11 +0700

    style(tailwind): change accent color to rose-600 and ensure danger and warning colors are defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,8 +19,10 @@ module.exports = {
       colors: {
         primary: "#14b8a6", // teal-500
         secondary: "#2dd4bf", // teal-600
-        accent: "#ec4899", // pink-500
+        accent: "#e11d48", // rose-600
         neutral: "#fbbc05", // yellow-500
+        danger: "#ef4444", // red-500
+        warning: "#f97316", // orange-500
       },
     },
   },
