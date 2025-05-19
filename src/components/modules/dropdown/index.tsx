73b5b6c016321470c855b449c8b0724bad4dfe94
fxrdhan <firdausarief65@gmail.com>
2025-05-19 20:19:34 +0700

    style(dropdown): add transition effect to search input for smoother interactions

--- src/components/modules/dropdown/index.tsx
+++ src/components/modules/dropdown/index.tsx
@@ -315,7 +315,7 @@ export const Dropdown = ({
                                                     <input
                                                         ref={searchInputRef}
                                                         type="text"
-                                                        className="flex-grow py-2 px-2 pl-8 text-sm border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-primary"
+                                                        className="flex-grow py-2 px-2 pl-8 text-sm border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-primary transition duration-200 ease-in-out"
                                                         placeholder="Cari..."
                                                         value={searchTerm}
                                                         onChange={(e) => setSearchTerm(e.target.value)}
