445d9cf5203ec68fc56f03728e11214194ce8df8
fxrdhan <firdausarief65@gmail.com>
2025-05-19 21:24:21 +0700

    style(input): update focus ring styles

--- src/components/modules/input/index.tsx
+++ src/components/modules/input/index.tsx
@@ -19,7 +19,7 @@ export const Input = ({
                 className={classNames(
                     'p-2 border rounded-lg',
                     error ? 'border-red-500' : 'border-gray-300',
-                    'focus:outline-none focus:border-primary focus:ring-2 focus:ring-teal-300',
+                    'focus:outline-none focus:border-primary focus:ring focus:ring-teal-100',
                     'disabled:bg-gray-100 disabled:cursor-not-allowed disabled:opacity-70 read-only:bg-gray-100 read-only:cursor-default read-only:opacity-70', // Disabled and Read-only styles
                     'disabled:focus:ring-0 disabled:focus:border-gray-300 read-only:focus:ring-0 read-only:focus:border-gray-300', // Disable focus styles when disabled/read-only
                     'transition duration-200 ease-in-out',
