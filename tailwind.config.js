30bd878e7be70367a854d57f680415e32663eeec
fxrdhan <firdausarief65@gmail.com>
2025-05-20 23:44:55 +0700

    chore(tailwind): remove react-tailwindcss-datepicker from content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.{js,ts}",
   ],
   theme: {
     extend: {
