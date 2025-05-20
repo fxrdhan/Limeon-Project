5c074886b86dd14ba5917132fe6421cee2902839
fxrdhan <firdausarief65@gmail.com>
2025-05-20 15:47:27 +0700

    refactor(types): move CustomDateValueType and PortalledDatepickerProps to types

--- src/pages/purchases/create-purchase/index.tsx
+++ src/pages/purchases/create-purchase/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaTrash } from 'react-icons/fa';
-import type { CustomDateValueType } from '@/components/modules/portalled-datepicker';
+import type { CustomDateValueType } from '@/types';
 
 import {
     Input,
