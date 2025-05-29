b54910ce4f785d2d37670c06f3ffbfaf9a00adce
fxrdhan <firdausarief65@gmail.com>
2025-05-30 00:23:50 +0700

    feat(SearchBar): enable inputRef prop for direct input access
    The SearchBar component is updated to accept and utilize an `inputRef` prop.
    This ref is forwarded to the native HTML input element, allowing parent
    components to directly control focus and other input-related behaviors.
    This is a key step towards implementing persistent focus on search fields
    across various list pages.

--- src/components/modules/search-bar/index.tsx
+++ src/components/modules/search-bar/index.tsx
@@ -6,14 +6,16 @@ export const SearchBar: React.FC<TableSearchProps> = ({
     value,
     onChange,
     placeholder = "Cari...",
-    className = ""
+    className = "",
+    inputRef
 }) => {
     return (
         <div className={`mb-4 relative ${className}`}>
             <input
+                ref={inputRef}
                 type="text"
                 placeholder={placeholder}
-                className="w-full p-2 border rounded-lg pl-10 focus:outline-none focus:border-primary focus:ring focus:ring-teal-100 transition duration-200 ease-in-out"
+                className="text-sm tracking-normal w-full p-2.5 border rounded-lg pl-10 focus:outline-none focus:border-primary focus:ring focus:ring-teal-100 transition duration-200 ease-in-out"
                 value={value}
                 onChange={onChange} />
             <FaSearch className="absolute left-3 top-3.5 text-gray-400" />
