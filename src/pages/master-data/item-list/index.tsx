ba5404ad58ad26f9d9a477cab71993d3a73c5b04
fxrdhan <firdausarief65@gmail.com>
2025-06-07 05:51:42 +0700

    fix(item-list): ensure unique key for AddItemPortal component

--- src/pages/master-data/item-list/index.tsx
+++ src/pages/master-data/item-list/index.tsx
@@ -226,6 +226,7 @@ function ItemList() {
                 )}
             </Card>
             <AddItemPortal
+                key={`${editingItemId ?? 'new'}-${currentSearchQueryForModal ?? ''}`}
                 isOpen={isAddItemModalOpen}
                 onClose={closeAddItemModal}
                 itemId={editingItemId}
