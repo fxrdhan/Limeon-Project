8e91f538bc98909f3f700e8a56f38ec04f65c736
fxrdhan <firdausarief65@gmail.com>
2025-07-18 03:17:12 +0700

    style(theme): remove accent color and update danger color
    - Remove the now unused accent color from the theme
    - Update the danger color to a new value

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,9 @@ export default {
       colors: {
         primary: "oklch(50.8% 0.118 165.612)",
         secondary: "oklch(59.6% 0.145 163.225)",
-        accent: "oklch(51.4% 0.222 16.935)",
+        // accent: "oklch(51.4% 0.222 16.935)",
         neutral: "oklch(79.5% 0.184 86.047)",
-        danger: "oklch(63.7% 0.237 25.331)",
+        danger: "oklch(57.7% 0.245 27.325)",
         warning: "oklch(70.5% 0.213 47.604)",
       },
     },
