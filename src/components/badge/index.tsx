d38048692cfd527a00284f3733b30f8cd269597a
fxrdhan <firdausarief65@gmail.com>
2025-07-18 06:43:18 +0700

    feat(badge): remove accent variant

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -13,7 +13,6 @@ const Badge = ({
     default: "bg-gray-100 text-gray-800 border-gray-200",
     primary: "bg-blue-100 text-blue-800",
     secondary: "bg-gray-100 text-gray-800",
-    accent: "bg-rose-100 text-rose-800",
     success: "bg-emerald-100 text-emerald-800",
     warning: "bg-yellow-100 text-yellow-800",
     danger: "bg-red-100 text-red-800",
