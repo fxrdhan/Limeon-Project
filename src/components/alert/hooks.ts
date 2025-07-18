af16f8769ce03f110411e37ac92062f7f826d889
fxrdhan <firdausarief65@gmail.com>
2025-07-18 07:48:11 +0700

    feat(alert): move AlertContext to hooks file and export it

--- src/components/alert/hooks.ts
+++ src/components/alert/hooks.ts
@@ -1,19 +1,22 @@
-import { useContext } from "react";
-import type { AlertHook } from "@/types";
-import { AlertContext } from "./AlertContext";
+import { useContext, createContext } from "react";
+import type { AlertHook, AlertContextType } from "@/types";
+
+export const AlertContext = createContext<AlertContextType | undefined>(
+  undefined,
+);
 
 export const useAlert = (): AlertHook => {
-    const context = useContext(AlertContext);
-    if (!context) {
-        throw new Error("useAlert harus digunakan di dalam AlertProvider");
-    }
+  const context = useContext(AlertContext);
+  if (!context) {
+    throw new Error("useAlert harus digunakan di dalam AlertProvider");
+  }
 
-    const { addAlert } = context;
+  const { addAlert } = context;
 
-    return {
-        success: (message, options) => addAlert(message, "success", options),
-        error: (message, options) => addAlert(message, "error", options),
-        warning: (message, options) => addAlert(message, "warning", options),
-        info: (message, options) => addAlert(message, "info", options),
-    };
+  return {
+    success: (message, options) => addAlert(message, "success", options),
+    error: (message, options) => addAlert(message, "error", options),
+    warning: (message, options) => addAlert(message, "warning", options),
+    info: (message, options) => addAlert(message, "info", options),
+  };
 };
