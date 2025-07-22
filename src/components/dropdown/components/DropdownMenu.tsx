9877542b962c8c52ada3555832301027ad012323
fxrdhan <firdausarief65@gmail.com>
2025-07-23 06:26:26 +0700

    refactor(dropdown): integrate DropdownMenu with useDropdownContext hook
    - Centralize state management by connecting DropdownMenu to the new useDropdownContext hook
    - Remove redundant prop drilling and simplify the components API
    - Increase maintainability by delegating state logic to the custom hook

--- src/components/dropdown/components/DropdownMenu.tsx
+++ src/components/dropdown/components/DropdownMenu.tsx
@@ -1,87 +1,38 @@
-import React, { forwardRef, CSSProperties, RefObject } from 'react';
+import { forwardRef, RefObject } from 'react';
 import MenuPortal from './menu/MenuPortal';
 import MenuContent from './menu/MenuContent';
 import SearchBar from './SearchBar';
 import OptionItem from './OptionItem';
 import EmptyState from './menu/EmptyState';
-import { DropDirection } from '../constants';
-
-interface DropdownMenuProps {
-  isOpen: boolean;
-  isClosing: boolean;
-  applyOpenStyles: boolean;
-  dropDirection: DropDirection;
-  portalStyle: CSSProperties;
-  searchList: boolean;
-  withRadio?: boolean;
-  searchTerm: string;
-  searchState: string;
-  currentFilteredOptions: Array<{ id: string; name: string }>;
-  highlightedIndex: number;
-  isKeyboardNavigation: boolean;
-  expandedId: string | null;
-  value?: string;
-  onAddNew?: (term: string) => void;
-  onSearchChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onSearchKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  onDropdownKeyDown?: (e: React.KeyboardEvent<HTMLElement>) => void;
-  onSelect: (optionId: string) => void;
-  onSetHighlightedIndex: (index: number) => void;
-  onSetIsKeyboardNavigation: (isKeyboard: boolean) => void;
-  onExpansion: (optionId: string, optionName: string, shouldExpand: boolean) => void;
-  onMenuEnter: () => void;
-  onMenuLeave: () => void;
-  onScroll: () => void;
-  buttonRef: RefObject<HTMLButtonElement | null>;
-  searchInputRef: RefObject<HTMLInputElement | null>;
-  optionsContainerRef: RefObject<HTMLDivElement | null>;
-  leaveTimeoutRef: RefObject<NodeJS.Timeout | null>;
-  scrollState: {
-    isScrollable: boolean;
-    reachedBottom: boolean;
-    scrolledFromTop: boolean;
-  };
-}
+import { useDropdownContext } from '../hooks/useDropdownContext';
+import type { DropdownMenuProps } from '../types';
 
 const DropdownMenu = forwardRef<HTMLDivElement, DropdownMenuProps>(
-  (
-    {
+  ({ leaveTimeoutRef, onSearchKeyDown }, ref) => {
+    const {
       isOpen,
       isClosing,
       applyOpenStyles,
       dropDirection,
       portalStyle,
       searchList,
-      withRadio,
       searchTerm,
       searchState,
-      currentFilteredOptions,
+      filteredOptions,
       highlightedIndex,
-      isKeyboardNavigation,
       expandedId,
       value,
       onAddNew,
-      onSearchChange,
-      onSearchKeyDown,
-      onDropdownKeyDown,
-      onSelect,
+      onKeyDown,
       onSetHighlightedIndex,
       onSetIsKeyboardNavigation,
-      onExpansion,
       onMenuEnter,
       onMenuLeave,
       onScroll,
-      buttonRef,
       searchInputRef,
       optionsContainerRef,
-      leaveTimeoutRef,
       scrollState,
-    },
-    ref,
-  ) => {
-    const handleAddNewFromSearch = (term: string) => {
-      onAddNew?.(term);
-    };
+    } = useDropdownContext();
 
     return (
       <MenuPortal
@@ -98,13 +49,6 @@ const DropdownMenu = forwardRef<HTMLDivElement, DropdownMenuProps>(
           {searchList && (
             <SearchBar
               ref={searchInputRef}
-              searchTerm={searchTerm}
-              searchState={searchState}
-              isOpen={isOpen}
-              highlightedIndex={highlightedIndex}
-              currentFilteredOptions={currentFilteredOptions}
-              onAddNew={handleAddNewFromSearch}
-              onSearchChange={onSearchChange}
               onKeyDown={onSearchKeyDown}
               onFocus={() => {}}
               leaveTimeoutRef={leaveTimeoutRef}
@@ -118,10 +62,10 @@ const DropdownMenu = forwardRef<HTMLDivElement, DropdownMenuProps>(
               tabIndex={-1}
               className="p-1 max-h-60 overflow-y-auto focus:outline-hidden"
               onScroll={onScroll}
-              onKeyDown={!searchList ? onDropdownKeyDown : undefined}
+              onKeyDown={!searchList ? onKeyDown : undefined}
             >
-              {currentFilteredOptions.length > 0 ? (
-                currentFilteredOptions.map((option, index) => (
+              {filteredOptions.length > 0 ? (
+                filteredOptions.map((option, index) => (
                   <OptionItem
                     key={option.id}
                     option={option}
@@ -129,15 +73,10 @@ const DropdownMenu = forwardRef<HTMLDivElement, DropdownMenuProps>(
                     isSelected={option.id === value}
                     isHighlighted={highlightedIndex === index}
                     isExpanded={expandedId === option.id}
-                    withRadio={withRadio}
-                    isKeyboardNavigation={isKeyboardNavigation}
-                    buttonRef={buttonRef}
-                    onSelect={onSelect}
                     onHighlight={(index) => {
                       onSetIsKeyboardNavigation(false);
                       onSetHighlightedIndex(index);
                     }}
-                    onExpansion={onExpansion}
                     dropdownMenuRef={ref as RefObject<HTMLDivElement>}
                   />
                 ))
