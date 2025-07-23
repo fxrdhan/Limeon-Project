d2e816d1a7d80a3682eeaad1116e07fa7c3f5683
fxrdhan <firdausarief65@gmail.com>
2025-07-23 08:41:46 +0700

    feat(dropdown): add new SearchIcon component

---
+++ src/components/dropdown/modules/search/SearchIcon.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
+import { getSearchIconColor } from '../../utils/dropdownUtils';
+
+interface SearchIconProps {
+  searchState: string;
+  hasSearchTerm: boolean;
+  position?: 'absolute' | 'relative';
+}
+
+const SearchIcon: React.FC<SearchIconProps> = ({
+  searchState,
+  hasSearchTerm,
+  position = 'absolute',
+}) => {
+  const iconColor = getSearchIconColor(searchState);
+  
+  if (hasSearchTerm && position === 'absolute') return null;
+
+  const baseClasses = `${iconColor} transition-all duration-300 ease-in-out`;
+  
+  const positionClasses = position === 'absolute' 
+    ? 'absolute top-2.5 right-2 opacity-100 transform translate-x-0'
+    : 'scale-125 ml-1 mr-1';
+
+  const sizeStyle = position === 'relative' 
+    ? { width: '16px', minWidth: '16px' }
+    : undefined;
+
+  return (
+    <FaMagnifyingGlass
+      className={`${baseClasses} ${positionClasses}`}
+      size={position === 'absolute' ? 16 : undefined}
+      style={sizeStyle}
+    />
+  );
+};
+
+export default SearchIcon;
