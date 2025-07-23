82ac9ff2c7fbec8ac2436a1039840e284c7e745d
fxrdhan <firdausarief65@gmail.com>
2025-07-23 09:30:59 +0700

    feat(datepicker): add hook for accessing datepicker context
    - provide a simple way to access the datepicker state
    - throw an error if used outside of the provider

---
+++ src/components/datepicker/hooks/useDatepickerContext.ts
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { DatepickerContext } from "../providers/datepickerContext";
+import type { DatepickerContextState } from "../types";
+
+export const useDatepickerContext = (): DatepickerContextState => {
+  const context = useContext(DatepickerContext);
+  
+  if (!context) {
+    throw new Error(
+      "useDatepickerContext must be used within a DatepickerProvider"
+    );
+  }
+  
+  return context;
+};
