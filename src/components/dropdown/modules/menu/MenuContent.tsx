43b0f064659036fce11a69942621bba4ae6df329
fxrdhan <firdausarief65@gmail.com>
2025-07-23 08:40:27 +0700

    feat(dropdown): add new MenuContent component

---
+++ src/components/dropdown/modules/menu/MenuContent.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from 'react';
+import ScrollIndicators from './ScrollIndicators';
+
+interface MenuContentProps {
+  children: ReactNode;
+  scrollState: {
+    isScrollable: boolean;
+    reachedBottom: boolean;
+    scrolledFromTop: boolean;
+  };
+}
+
+const MenuContent: React.FC<MenuContentProps> = ({
+  children,
+  scrollState,
+}) => {
+  return (
+    <div className="relative">
+      {children}
+      <ScrollIndicators
+        isScrollable={scrollState.isScrollable}
+        scrolledFromTop={scrollState.scrolledFromTop}
+        reachedBottom={scrollState.reachedBottom}
+      />
+    </div>
+  );
+};
+
+export default MenuContent;
