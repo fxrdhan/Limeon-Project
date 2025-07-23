f4f1121ec655b0f649e033be8801e0f9a532b27b
fxrdhan <firdausarief65@gmail.com>
2025-07-23 08:43:14 +0700

    feat(pagination): add new index file for pagination modules

---
+++ src/components/pagination/modules/index.ts
@@ -0,0 +1,5 @@
+export { PaginationButton } from './PaginationButton';
+export { PageSizeSelector } from './PageSizeSelector';
+export { CurrentPageDisplay } from './CurrentPageDisplay';
+export { PaginationContent } from './PaginationContent';
+export { FloatingWrapper } from './FloatingWrapper';
