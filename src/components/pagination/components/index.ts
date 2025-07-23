6050c44c0a8529c83bc0674a7df3bd381eb3e862
fxrdhan <firdausarief65@gmail.com>
2025-07-23 08:38:47 +0700

    refactor(components): relocate component modules to new directory structure

--- src/components/pagination/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { PaginationButton } from './PaginationButton';
-export { PageSizeSelector } from './PageSizeSelector';
-export { CurrentPageDisplay } from './CurrentPageDisplay';
-export { PaginationContent } from './PaginationContent';
-export { FloatingWrapper } from './FloatingWrapper';
