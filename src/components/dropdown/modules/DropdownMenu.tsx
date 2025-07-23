44033d4d43d9b4072a6e34045565a42f2b408e5c
fxrdhan <firdausarief65@gmail.com>
2025-07-23 08:39:10 +0700

    feat(dropdown): add new DropdownMenu component

---
+++ src/components/dropdown/modules/DropdownMenu.tsx
@@ -0,0 +1,100 @@
+import { forwardRef, RefObject } from 'react';
+import MenuPortal from './menu/MenuPortal';
+import MenuContent from './menu/MenuContent';
+import SearchBar from './SearchBar';
+import OptionItem from './OptionItem';
+import EmptyState from './menu/EmptyState';
+import { useDropdownContext } from '../hooks/useDropdownContext';
+import type { DropdownMenuProps } from '../types';
+
+const DropdownMenu = forwardRef<HTMLDivElement, DropdownMenuProps>(
+  ({ leaveTimeoutRef, onSearchKeyDown }, ref) => {
+    const {
+      isOpen,
+      isClosing,
+      applyOpenStyles,
+      dropDirection,
+      portalStyle,
+      searchList,
+      searchTerm,
+      searchState,
+      filteredOptions,
+      highlightedIndex,
+      expandedId,
+      value,
+      onAddNew,
+      onKeyDown,
+      onSetHighlightedIndex,
+      onSetIsKeyboardNavigation,
+      onMenuEnter,
+      onMenuLeave,
+      onScroll,
+      searchInputRef,
+      optionsContainerRef,
+      scrollState,
+    } = useDropdownContext();
+
+    return (
+      <MenuPortal
+        ref={ref}
+        isOpen={isOpen}
+        isClosing={isClosing}
+        applyOpenStyles={applyOpenStyles}
+        dropDirection={dropDirection}
+        portalStyle={portalStyle}
+        onMouseEnter={onMenuEnter}
+        onMouseLeave={onMenuLeave}
+      >
+        <div>
+          {searchList && (
+            <SearchBar
+              ref={searchInputRef}
+              onKeyDown={onSearchKeyDown}
+              onFocus={() => {}}
+              leaveTimeoutRef={leaveTimeoutRef}
+            />
+          )}
+          <MenuContent scrollState={scrollState}>
+            <div
+              id="dropdown-options-list"
+              ref={optionsContainerRef}
+              role="listbox"
+              tabIndex={-1}
+              className="p-1 max-h-60 overflow-y-auto focus:outline-hidden"
+              onScroll={onScroll}
+              onKeyDown={!searchList ? onKeyDown : undefined}
+            >
+              {filteredOptions.length > 0 ? (
+                filteredOptions.map((option, index) => (
+                  <OptionItem
+                    key={option.id}
+                    option={option}
+                    index={index}
+                    isSelected={option.id === value}
+                    isHighlighted={highlightedIndex === index}
+                    isExpanded={expandedId === option.id}
+                    onHighlight={(index) => {
+                      onSetIsKeyboardNavigation(false);
+                      onSetHighlightedIndex(index);
+                    }}
+                    dropdownMenuRef={ref as RefObject<HTMLDivElement>}
+                  />
+                ))
+              ) : (
+                <EmptyState
+                  searchState={searchState}
+                  searchTerm={searchTerm}
+                  hasAddNew={!!onAddNew}
+                />
+              )}
+            </div>
+          </MenuContent>
+        </div>
+      </MenuPortal>
+    );
+  },
+);
+
+DropdownMenu.displayName = 'DropdownMenu';
+
+export default DropdownMenu;
